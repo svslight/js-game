abe3b8aec82408bb2928fb7fb7879f3fe9adb639
Svetlana Solodovnikova <svslight@yandex.ru>
2019-05-13 13:01:36 +0500

    Исправленный код svs

--- game.js
+++ game.js
@@ -356,7 +356,7 @@ class Coin extends Actor {
 /* Игрок. 
 Класс Player - наследует Actor - содержит базовый функционал движущегося объекта.
 */   
-
+   
 class Player extends Actor {
   constructor(pos = new Vector(0, 0)) {
 	super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5), new Vector(0, 0));
